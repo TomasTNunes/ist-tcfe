3eca819bf2735a4315ce39b2b929a2a442f8c144
TomasTNunes <tomastrindadenunes@outlook.com>
2021-03-31 20:16:44 +0100

    Update 3 t2

--- t2/mat/t2.m
+++ t2/mat/t2.m
@@ -4,27 +4,26 @@ clear all
 format long;
 pkg load symbolic;
 
-%%%%%%%%%%%%%%%%%%%READ AND WRITE DATA FILES%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%READ AND WRITE DATA FILE DATA_1_NG.TXT%%%%%%%%%%%%%%%%%%%
 
 dataf = fopen('data.txt','r');
 DATA = fscanf(dataf,'%*s = %f');
-#DATA
 fclose(dataf);
 
-data_ngf = fopen('data_ng.txt','w');
-fprintf(data_ngf,'R1 1 2 %.11fk\n', DATA(1));
-fprintf(data_ngf,'R2 2 3 %.11fk\n', DATA(2));
-fprintf(data_ngf,'R3 2 4 %.11fk\n', DATA(3));
-fprintf(data_ngf,'R4 4 0 %.11fk\n', DATA(4));
-fprintf(data_ngf,'R5 4 5 %.11fk\n', DATA(5));
-fprintf(data_ngf,'R6 0 8 %.11fk\n', DATA(6));
-fprintf(data_ngf,'R7 6 7 %.11fk\n', DATA(7));
-fprintf(data_ngf,'C 5 7 %.11fu\n', DATA(9));
-fprintf(data_ngf,'Vs 1 0 %.11f\n', DATA(8));
-fprintf(data_ngf,'Vaux 8 6 DC 0\n');
-fprintf(data_ngf,'Hd 4 7 vaux %.11fk\n', DATA(11));
-fprintf(data_ngf,'Gb 5 3 (2,4) %.11fm\n', DATA(10));
-fclose(data_ngf);
+data_ngf1 = fopen('data_1_ng.txt','w');
+fprintf(data_ngf1,'R1 1 2 %.11fk\n', DATA(1));
+fprintf(data_ngf1,'R2 2 3 %.11fk\n', DATA(2));
+fprintf(data_ngf1,'R3 2 4 %.11fk\n', DATA(3));
+fprintf(data_ngf1,'R4 4 0 %.11fk\n', DATA(4));
+fprintf(data_ngf1,'R5 4 5 %.11fk\n', DATA(5));
+fprintf(data_ngf1,'R6 0 8 %.11fk\n', DATA(6));
+fprintf(data_ngf1,'R7 6 7 %.11fk\n', DATA(7));
+fprintf(data_ngf1,'C 5 7 %.11fu\n', DATA(9));
+fprintf(data_ngf1,'Vs 1 0 DC %.11f\n', DATA(8));
+fprintf(data_ngf1,'Vaux 8 6 DC 0\n');
+fprintf(data_ngf1,'Hd 4 7 vaux %.11fk\n', DATA(11));
+fprintf(data_ngf1,'Gb 5 3 (2,4) %.11fm\n', DATA(10));
+fclose(data_ngf1);
 
 %%%%%%%%%%%%%%%%%%%SYMBOLIC COMPUTATIONS%%%%%%%%%%%%%%%%%%%
 
@@ -105,6 +104,23 @@ Eqd_Vx = V5n-V7n == Vx;
 Eqd_Vs_2 = V1n == Vs;
 sn_2 = solve(Eqd_2,Eqd_0_R6,Eqd_0_R7,Eqd_Vs_2,Eqd_Vd,Eqd_Vx,Eqd_3);
 
+%%%%%Nodal method 2) WRITE FILE DATA_2_NG.TXT%%%%%
+
+data_ngf2 = fopen('data_2_ng.txt','w');
+fprintf(data_ngf2,'R1 1 2 %.11fk\n', DATA(1));
+fprintf(data_ngf2,'R2 2 3 %.11fk\n', DATA(2));
+fprintf(data_ngf2,'R3 2 4 %.11fk\n', DATA(3));
+fprintf(data_ngf2,'R4 4 0 %.11fk\n', DATA(4));
+fprintf(data_ngf2,'R5 4 5 %.11fk\n', DATA(5));
+fprintf(data_ngf2,'R6 0 8 %.11fk\n', DATA(6));
+fprintf(data_ngf2,'R7 6 7 %.11fk\n', DATA(7));
+fprintf(data_ngf2,'Vx 5 7 DC %.6f\n',double(Vx));
+fprintf(data_ngf2,'Vs 1 0 DC 0\n');
+fprintf(data_ngf2,'Vaux 8 6 DC 0\n');
+fprintf(data_ngf2,'Hd 4 7 vaux %.11fk\n', DATA(11));
+fprintf(data_ngf2,'Gb 5 3 (2,4) %.11fm\n', DATA(10));
+fclose(data_ngf2);
+
 %%%%%Nodal method 2) NUMERIC%%%%%
 
 V1n = double(sn_2.V1n);
@@ -132,7 +148,25 @@ tau = double(C) * 10^-6 * Req*1000;
 
 diary "Req_tau_tab.tex"
 diary on
-printf("$R_e$$_q$ & %f\n",Req);
+printf("$I_b$ & %f\n", Ibn/1000);
+printf("$I_R$$_1$ & %f\n",IR1n/1000);
+printf("$I_R$$_2$ & %f\n",IR2n/1000);
+printf("$I_R$$_3$ & %f\n",IR3n/1000);
+printf("$I_R$$_4$ & %f\n",IR4n/1000);
+printf("$I_R$$_5$ & %e\n",IR5n/1000);
+printf("$I_R$$_6$ & %f\n",IR6n/1000);
+printf("$I_R$$_7$ & %f\n",IR7n/1000);
+printf("$V_1$ & %f\n",V1n);
+printf("$V_2$ & %f\n",V2n);
+printf("$V_3$ & %f\n",V3n);
+printf("$V_4$ & %f\n",V4n);
+printf("$V_5$ & %f\n",V5n);
+printf("$V_6$ & %f\n",V6n);
+printf("$V_7$ & %f\n",V7n);
+printf("$V_8$ & %f\n",V8n);
+printf("$V_X$ & %f\n",double(Vx));
+printf("$I_X$ & %e\n",Ix/1000);
+printf("$R_e$$_q$ & %f\n",Req*1000);
 printf("$tau$ & %e\n",tau);
 diary off
 
@@ -143,23 +177,40 @@ syms A s
 
 vc_n(t) = A*exp(s*t);
 
+%%%%%NATURAL SOLUTION 3) WRITE FILE DATA_3_NG.TXT%%%%%
+
+data_ngf3 = fopen('data_3_ng.txt','w');
+fprintf(data_ngf3,'R1 1 2 %.11fk\n', DATA(1));
+fprintf(data_ngf3,'R2 2 3 %.11fk\n', DATA(2));
+fprintf(data_ngf3,'R3 2 4 %.11fk\n', DATA(3));
+fprintf(data_ngf3,'R4 4 0 %.11fk\n', DATA(4));
+fprintf(data_ngf3,'R5 4 5 %.11fk\n', DATA(5));
+fprintf(data_ngf3,'R6 0 8 %.11fk\n', DATA(6));
+fprintf(data_ngf3,'R7 6 7 %.11fk\n', DATA(7));
+fprintf(data_ngf3,'C 5 7 %.11fu\n', DATA(9));
+fprintf(data_ngf3,'Vs 1 0 DC 0\n');
+fprintf(data_ngf3,'Vaux 8 6 DC 0\n');
+fprintf(data_ngf3,'Hd 4 7 vaux %.11fk\n', DATA(11));
+fprintf(data_ngf3,'Gb 5 3 (2,4) %.11fm\n', DATA(10));
+fclose(data_ngf3);
+
 %%%%%NATURAL SOLUTION 3) NUMERIC%%%%%
 
-A = double(Vx); %initial condition V6(0) = Vx
+A = double(Vx); %initial condition V5(0) = Vx
 s = 1/tau; %pois Req e negativo logo tau e negativo
 
 t=0:1e-6:20e-3;
 vc_n = A*exp(s*t);
-v6_n = vc_n;
+v5_n = vc_n;
 
 %%%%%NATURAL SOLUTION 3) PLOT%%%%%
 
 hf = figure (1);
-plot (t*1000, v6_n, "g");
+plot (t*1000, v5_n, "g");
 xlabel ("t [ms]");
-ylabel ("v6_n [V]");
-legend('v6_n(t)','Location','northeast');
-print (hf, "v6_n.eps", "-depsc");
+ylabel ("v5_n [V]");
+legend('v5_n(t)','Location','northeast');
+print (hf, "v5_n.eps", "-depsc");
 
 %%%%%FORCED SOLUTION 4)%%%%%
 
