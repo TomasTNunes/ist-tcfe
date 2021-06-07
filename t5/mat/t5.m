b169d01decf733346fd1b5d6def5ca3d321022e2
TomasTNunes <tomastrindadenunes@outlook.com>
2021-06-07 01:02:46 +0100

    Update 2 t5

--- t5/mat/t5.m
+++ t5/mat/t5.m
@@ -1,10 +1,10 @@
 clear all
 
 R1 = 1000
-R2 = 10000
-R3 = 100000
+R2 = 1000
+R3 = 300000
 R4 = 1000
-C1 = 220e-9
+C1 = 110e-9
 C2 = 220e-9
 
 
@@ -27,9 +27,9 @@ title("Phase");
 print(f2, "teo_phase.eps", "-depsc");
 
 
-wL = 1/(R1*C1)
+wL = 1/(R1*C2)
 fL = wL/(2*pi)
-wH = 1/(R2*C2)
+wH = 1/(R2*C1)
 fH = wH/(2*pi)
 wO = sqrt(wL*wH)
 fO = wO/(2*pi)
@@ -40,7 +40,38 @@ gain_db = 20*log10(abs(gain))
 Z_in = abs(R1 + 1/(j*wO*C1))
 Z_out = abs(1/(j*wO*C2+1/R2))
 
-Cost = (R1+R2+R3+R4)/1000 + (C1+C2)*1000000 + 13323
+Cost = (R1+R2+R3+R4)/1000 + (4*C1+C2)*1000000 + 13323
 gain_deviation = abs(100-gain)
 frequency_deviation = abs(fO-1000)
 Merit = 1/(Cost*(gain_deviation+frequency_deviation+10^(-6)))
+
+
+
+diary "data1_teo.tex"
+diary on
+printf("$f_{L}$ & %.2f\n", fL);
+printf("$f_{H}$ & %.2f\n", fH);
+printf("$f_{O}$ & %.2f\n", fO);
+diary off
+
+
+diary "data2_teo.tex"
+diary on
+printf("$gain$ & %.2f\n", gain);
+printf("$gain_{dB}$ & %.3f\n", gain_db);
+printf("$Z_{IN}$ & %.2f\n", Z_in);
+printf("$Z_{OUT}$ & %.2f\n", Z_out)
+diary off
+
+
+
+diary "data3_teo.tex"
+diary on
+printf("$gain_dev$ & %.6f\n", gain_deviation);
+printf("$freq_dev$ & %.4f\n", frequency_deviation);
+printf("$Cost$ & %.2f\n", Cost);
+printf("$Merit$ & %.6e\n", Merit);
+diary off
+
+
+
